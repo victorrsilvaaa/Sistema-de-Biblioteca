c0fa5025c26422d42e80c30a649517499015b496
Joao Victor <victorsantos.pe2025@gmail.com>
2026-05-01 23:43:37 -0300

    criação do método pegar emprestado

--- src/SistemaBiblioteca.java
+++ src/SistemaBiblioteca.java
@@ -20,99 +20,104 @@ public class SistemaBiblioteca {
             System.out.println(a);
 
         }
+
+    }
+
+    public static void pegarEmprestado(Scanner sc, ArrayList<Livro> livros) {
+        System.out.println("Digite o Titulo do Livro que Deseja Pegar Emprestado:");
+        String emprestado = sc.nextLine();
+
+        for (Livro a : livros) {
+
+            if (emprestado.equalsIgnoreCase(a.getTintulo())) {
+                if (a.isEmprestado()) {
+                    System.out.println("Livro encontrado, porem já foi emprestado!");
+                    break;
+                }
+
+                a.setEmprestado(true);
+                System.out.println("Livro: " + a.getTintulo() + " Emprestado!");
+
+            }
+        }
+        System.out.println("Livro Não encontrado");
+
     }
 
 
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        ArrayList<Livro> livros = new ArrayList<>();
 
 
-        System.out.println("ESCOLHA UMA DAS OPÇÕES PARA ACESSO A BIBLIOTECA");
+        public static void main (String[]args){
 
-        int opcao = 0;
+            Scanner sc = new Scanner(System.in);
 
-        while (opcao != 6) {
-
-            System.out.println("1- Cadastrar livro");
-            System.out.println("2- Listar Livros");
-            System.out.println("3- Emprestar Livros");
-            System.out.println("4- Devolver Livros");
-            System.out.println("5- Buscar Livros");
-            System.out.println("6- Sair");
-            System.out.println();
-            System.out.println("Escolha a opção:");
-
-            opcao = sc.nextInt();
-            sc.nextLine();
+            ArrayList<Livro> livros = new ArrayList<>();
 
 
-            switch (opcao) {
-                case 1:
-                    cadastrarLivro(sc, livros);
-                    break;
-                case 2:
-                    listarLivros(livros);
-                    break;
+            System.out.println("ESCOLHA UMA DAS OPÇÕES PARA ACESSO A BIBLIOTECA");
 
-                case 3:
-                    System.out.println("Digite o Titulo do Livro que Deseja Pegar Emprestado:");
-                    String emprestado = sc.nextLine();
+            int opcao = 0;
 
-                    for (Livro a : livros) {
+            while (opcao != 6) {
 
-                        if (emprestado.equalsIgnoreCase(a.getTintulo())) {
-                            if (a.isEmprestado()) {
-                                System.out.println("Livro encontrado, porem já foi emprestado!");
-                                break;
-                            }
+                System.out.println("1- Cadastrar livro");
+                System.out.println("2- Listar Livros");
+                System.out.println("3- Emprestar Livros");
+                System.out.println("4- Devolver Livros");
+                System.out.println("5- Buscar Livros");
+                System.out.println("6- Sair");
+                System.out.println();
+                System.out.println("Escolha a opção:");
 
-                            a.setEmprestado(true);
-                            System.out.println("Livro: " + a.getTintulo() + " Emprestado!");
+                opcao = sc.nextInt();
+                sc.nextLine();
 
-                            break;
-                        }
-                        System.out.println("Livro Não encontrado");
 
+                switch (opcao) {
+                    case 1:
+                        cadastrarLivro(sc, livros);
+                        break;
+                    case 2:
+                        listarLivros(livros);
+                        break;
+
+                    case 3:
+                        pegarEmprestado(sc, livros);
+                        break;
+            case 4:
+                System.out.println("Digite o nome do Livro que deseja devolver:");
+                String devolucao = sc.next();
+
+                for (Livro d : livros) {
+                    if (devolucao.equalsIgnoreCase(d.getTintulo())) {
+                        d.setEmprestado(false);
+                        System.out.println("Devolucao feita!");
+                        break;
                     }
-                    break;
+                }
 
-                case 4:
-                    System.out.println("Digite o nome do Livro que deseja devolver:");
-                    String devolucao = sc.next();
+                System.out.println("Livro que deseja devolver não encontrado!");
+                break;
 
-                    for (Livro d : livros) {
-                        if (devolucao.equalsIgnoreCase(d.getTintulo())) {
-                            d.setEmprestado(false);
-                            System.out.println("Devolucao feita!");
-                            break;
-                        }
-                    }
+            case 5:
+                System.out.println("Livros Encontrados:");
 
-                    System.out.println("Livro que deseja devolver não encontrado!");
-                    break;
+                for (Livro a : livros) {
+                    System.out.println(a.getTintulo());
+                }
+                break;
 
-                case 5:
-                    System.out.println("Livros Encontrados:");
+            case 6:
+                System.out.println("Saindo da biblioteca!");
+                break;
 
-                    for (Livro a : livros) {
-                        System.out.println(a.getTintulo());
-                    }
-                    break;
-
-                case 6:
-                    System.out.println("Saindo da biblioteca!");
-                    break;
-
-                default:
-                    System.out.println("Opção invalida, escolha novamente!");
-                    System.out.println();
-                    break;
-            }
-
+            default:
+                System.out.println("Opção invalida, escolha novamente!");
+                System.out.println();
+                break;
         }
 
     }
+
+}
 }
